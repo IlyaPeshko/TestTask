2e58f17a20e1c6d3f7ef893fceb60930b2d931f7
IlyaPeshko <mr.ilyapeshko@icloud.com>
2017-08-25 03:33:44 +0300

    Update SearchPage.java

--- src/main/java/by/htp/task/ui/page/task_1/SearchPage.java
+++ src/main/java/by/htp/task/ui/page/task_1/SearchPage.java
@@ -1,36 +1,41 @@
 package by.htp.task.ui.page.task_1;
 
+import by.htp.task.ui.page.Page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import java.util.List;
 
-public class SearchPage extends Page{
+public class SearchPage extends Page {
     private static final By resultElement = By.xpath("//*[@class='b-results-list']/li/h3/a[2]");
-    private static String target = "Minsk Automated Testing Community";
-    private boolean founded = false;
+    private boolean isFounded = false;
 
-    public boolean step2 (WebDriver webDriver) throws WebDriverException{
-        logger("searing: Minsk Automated Testing Community");
+    public SearchPage(WebDriver driver) {
+        super(driver);
+    }
 
-        List<WebElement> list = webDriver.findElements(resultElement);
+    public boolean findOnPage (String target) throws WebDriverException{
+
+        List<WebElement> list = driver.findElements(resultElement);
         for (WebElement element : list) {
-            if (element.getText().equals(target)){
+            if (element.getText().equals(target) & element.isEnabled()){
                 element.click();
-                founded = true;
+                isFounded = true;
             }
         }
 
-        logger("results: " +list.size());
+        logger("founded: " +list.size());
+        logger("searching on page: " + target + " / " + isFounded);
 
         try{
-            if (!founded){
+            if (!isFounded){
                 throw new WebDriverException ("Minsk Automated Testing Community - not found");
             }
         }catch (WebDriverException e){
             logger(e);
         }
-        return (!list.isEmpty());
+
+        return (isFounded);
     }
 }
